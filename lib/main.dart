4994a16a0adc44d82da30fdd2b796ad95cf8953e
quang032002 <tranminhquang10a3@gmail.com>
2022-11-12 23:06:31 +0700

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo asdss',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,4 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
