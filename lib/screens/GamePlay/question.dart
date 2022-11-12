6fd3e35dceedd6c43412c1437d0c1b094f5f3dca
zCoLz <locpg2002@gmail.com>
2022-11-12 17:21:05 +0700

    update

--- lib/screens/GamePlay/question.dart
+++ lib/screens/GamePlay/question.dart
@@ -29,114 +29,114 @@ class _QuestionState extends State<Question> {
       );
     }
 
-    return Container(  
-      decoration: BoxDecoration(
-        image: DecorationImage(
-        image: AssetImage("acssets/background2.jpg"),
-      fit: BoxFit.cover)),
-      child:Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'Sport',
-            style: TextStyle(
-                color: Color.fromARGB(255, 55, 55, 55),
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
-          ),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.chevron_left,
-                color: Colors.white,
-                size: 40,
-              )),
-        ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          margin: EdgeInsets.fromLTRB(20, 50, 20, 10),
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Column(children: [
-                  Row(
-                    children: [
-                      Expanded(
-                          child: Text(
-                        'Điểm 00',
-                        style: TextStyle(fontSize: 23),
-                      )),
-                      Text(
-                        'Thời gian : 30s',
-                        style: TextStyle(fontSize: 23),
-                      )
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 30, 0, 50),
-                    child: Text(
-                      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buttonAnswer('Đáp án 1'),
-                      buttonAnswer('Đáp án 2')
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      buttonAnswer('Đáp án 3'),
-                      buttonAnswer('Đáp án 4')
-                    ],
-                  ),
-                ]),
+    return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("acssets/background2.jpg"),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                'Sport',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 55, 55, 55),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
               ),
-              Container(
-                  margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          child: Row(
+              leading: IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.chevron_left,
+                    color: Colors.white,
+                    size: 40,
+                  )),
+            ),
+            body: Container(
+              height: MediaQuery.of(context).size.height,
+              margin: EdgeInsets.fromLTRB(20, 50, 20, 10),
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(children: [
+                      Row(
                         children: [
-                          Icon(
-                            Icons.monetization_on_rounded,
-                            color: Colors.yellow,
-                            size: 30,
-                          ),
+                          Expanded(
+                              child: Text(
+                            'Điểm 00',
+                            style: TextStyle(fontSize: 23),
+                          )),
                           Text(
-                            '100',
-                            style: TextStyle(fontSize: 18),
-                          ),
-                          Padding(padding: EdgeInsets.only(left: 15)),
-                          Icon(
-                            Icons.diamond_rounded,
-                            size: 30,
-                            color: Colors.red,
-                          ),
-                          Text(
-                            '100',
-                            style: TextStyle(fontSize: 18),
+                            'Thời gian : 30s',
+                            style: TextStyle(fontSize: 23),
                           )
                         ],
-                      )),
-                      Icon(
-                        Icons.info_sharp,
-                        size: 40,
-                      )
-                    ],
-                  ))
-            ],
-          ),
-        )));
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 30, 0, 50),
+                        child: Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          buttonAnswer('Đáp án 1'),
+                          buttonAnswer('Đáp án 2')
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          buttonAnswer('Đáp án 3'),
+                          buttonAnswer('Đáp án 4')
+                        ],
+                      ),
+                    ]),
+                  ),
+                  Container(
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Row(
+                            children: [
+                              Icon(
+                                Icons.monetization_on_rounded,
+                                color: Colors.yellow,
+                                size: 30,
+                              ),
+                              Text(
+                                '100',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Padding(padding: EdgeInsets.only(left: 15)),
+                              Icon(
+                                Icons.diamond_rounded,
+                                size: 30,
+                                color: Colors.red,
+                              ),
+                              Text(
+                                '100',
+                                style: TextStyle(fontSize: 18),
+                              )
+                            ],
+                          )),
+                          Icon(
+                            Icons.info_sharp,
+                            size: 40,
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            )));
   }
 }
--- lib/screens/home_profile.dart
+++ lib/screens/home_profile.dart
@@ -11,131 +11,145 @@ class PageProfile extends StatefulWidget {
 class _PageProfileState extends State<PageProfile> {
   TextEditingController _controller = TextEditingController();
   @override
-  
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(""),
-          actions: [
-            IconButton(
-                onPressed: (() {}), icon: Icon(Icons.power_settings_new)),
-          ],
-        ),
-        drawer: PageDrawer(),
-        
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("acssets/background2.jpg"),
-                    fit: BoxFit.cover)),
-            child: Column(children: [
-            Icon(Icons.account_circle,size: 60,),
-            Text("Quang Thiếu U",style: TextStyle(fontSize: 30),),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _controller,
-                keyboardType: TextInputType.text,
-                
-                style: TextStyle(fontSize: 20,color: Colors.black),
-                decoration: InputDecoration(labelText: "Số điện thoại",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),)),
-                
-              ),
-            ),Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 20,color: Colors.black),
-                decoration: InputDecoration(labelText: "Số điện thoại",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),)),
-                
-              ),
-            ),Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 20,color: Colors.black),
-                decoration: InputDecoration(labelText: "Số điện thoại",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20),)),
-                
-              ),
-            )
-          ]),),
-        )
-        //Column(children: [
-        //     Icon(Icons.account_circle),
-        //     Text("Quang Thiếu U"),
-        //     Container(
-        //         child: Padding(
-        //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        //       child: TextField(
-        //         obscureText: false,
-        //         textAlign: TextAlign.left,
-        //         decoration: InputDecoration(
-        //             border: OutlineInputBorder(),
-        //             hintText: 'Số điện thoại',
-        //             hintStyle: TextStyle(
-        //                 color: Colors.grey,
-        //                 fontSize: 16,
-        //                 fontStyle: FontStyle.italic,
-        //                 fontWeight: FontWeight.bold)),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("acssets/background2.jpg"), fit: BoxFit.fill)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+          resizeToAvoidBottomInset: true,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(""),
+            actions: [
+              IconButton(
+                  onPressed: (() {}), icon: Icon(Icons.power_settings_new)),
+            ],
+          ),
+          drawer: PageDrawer(),
+          body: Center(
+            child: Container(
+              child: Column(children: [
+                Icon(
+                  Icons.account_circle,
+                  size: 60,
+                ),
+                Text(
+                  "Quang Thiếu U",
+                  style: TextStyle(fontSize: 30),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _controller,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                    decoration: InputDecoration(
+                        labelText: "Số điện thoại",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                    decoration: InputDecoration(
+                        labelText: "Số điện thoại",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                    decoration: InputDecoration(
+                        labelText: "Số điện thoại",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        )),
+                  ),
+                )
+              ]),
+            ),
+          )
+          //Column(children: [
+          //     Icon(Icons.account_circle),
+          //     Text("Quang Thiếu U"),
+          //     Container(
+          //         child: Padding(
+          //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          //       child: TextField(
+          //         obscureText: false,
+          //         textAlign: TextAlign.left,
+          //         decoration: InputDecoration(
+          //             border: OutlineInputBorder(),
+          //             hintText: 'Số điện thoại',
+          //             hintStyle: TextStyle(
+          //                 color: Colors.grey,
+          //                 fontSize: 16,
+          //                 fontStyle: FontStyle.italic,
+          //                 fontWeight: FontWeight.bold)),
 
-        //       ),
+          //       ),
 
-        //     )),
-        //     Container(
-        //         child: Padding(
-        //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        //       child: TextField(
-        //         obscureText: false,
-        //         textAlign: TextAlign.left,
-        //         decoration: InputDecoration(
-        //             border: OutlineInputBorder(),
-        //             hintText: '0351511xxx',
-        //             hintStyle: TextStyle(
-        //                 color: Colors.grey,
-        //                 fontSize: 16,
-        //                 fontStyle: FontStyle.italic,
-        //                 fontWeight: FontWeight.bold)),
-        //       ),
-        //     )),
-        //     Container(
-        //         child: Padding(
-        //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        //       child: TextField(
-        //         obscureText: false,
-        //         textAlign: TextAlign.left,
-        //         decoration: InputDecoration(
-        //             border: OutlineInputBorder(),
-        //             hintText: 'Số điện thoại',
-        //             hintStyle: TextStyle(
-        //                 color: Colors.grey,
-        //                 fontSize: 16,
-        //                 fontStyle: FontStyle.italic,
-        //                 fontWeight: FontWeight.bold)),
-        //       ),
-        //     )),
-        //     Container(
-        //         child: Padding(
-        //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-        //       child: TextField(
-        //         obscureText: false,
-        //         textAlign: TextAlign.left,
-        //         decoration: InputDecoration(
-        //             border: OutlineInputBorder(),
-        //             hintText: 'quangthieu@gmail',
-        //             hintStyle: TextStyle(
-        //                 color: Colors.grey,
-        //                 fontSize: 16,
-        //                 fontStyle: FontStyle.italic,
-        //                 fontWeight: FontWeight.bold)),
-        //       ),
-        //     )),
-        //   ]),
-        );
+          //     )),
+          //     Container(
+          //         child: Padding(
+          //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          //       child: TextField(
+          //         obscureText: false,
+          //         textAlign: TextAlign.left,
+          //         decoration: InputDecoration(
+          //             border: OutlineInputBorder(),
+          //             hintText: '0351511xxx',
+          //             hintStyle: TextStyle(
+          //                 color: Colors.grey,
+          //                 fontSize: 16,
+          //                 fontStyle: FontStyle.italic,
+          //                 fontWeight: FontWeight.bold)),
+          //       ),
+          //     )),
+          //     Container(
+          //         child: Padding(
+          //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          //       child: TextField(
+          //         obscureText: false,
+          //         textAlign: TextAlign.left,
+          //         decoration: InputDecoration(
+          //             border: OutlineInputBorder(),
+          //             hintText: 'Số điện thoại',
+          //             hintStyle: TextStyle(
+          //                 color: Colors.grey,
+          //                 fontSize: 16,
+          //                 fontStyle: FontStyle.italic,
+          //                 fontWeight: FontWeight.bold)),
+          //       ),
+          //     )),
+          //     Container(
+          //         child: Padding(
+          //       padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          //       child: TextField(
+          //         obscureText: false,
+          //         textAlign: TextAlign.left,
+          //         decoration: InputDecoration(
+          //             border: OutlineInputBorder(),
+          //             hintText: 'quangthieu@gmail',
+          //             hintStyle: TextStyle(
+          //                 color: Colors.grey,
+          //                 fontSize: 16,
+          //                 fontStyle: FontStyle.italic,
+          //                 fontWeight: FontWeight.bold)),
+          //       ),
+          //     )),
+          //   ]),
+          ),
+    );
   }
 }
