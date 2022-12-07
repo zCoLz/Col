07d32be59e5cb857802d24c3fdc5bbf0dad8f371
zCoLz <locpg2002@gmail.com>
2022-12-07 07:55:47 +0700

    update

---
+++ lib/components/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+const Color correct = Color.fromARGB(229, 7, 200, 46);
+
+const Color incorrect = Color.fromARGB(228, 217, 33, 33);
+
+const Color neutral = Color.fromARGB(226, 252, 252, 252);
